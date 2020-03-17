906c6f2d18c2380b9a1f5497deb8060dfcf4aefa
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-17 18:00:50 +0530

    create the form view

--- Tutorial/application/views/main_view.php
+++ Tutorial/application/views/main_view.php
@@ -7,9 +7,20 @@
 		<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
 	</head>
 	<body>
-			<div class="container">
+			<div class="container pt-5">
 				<h3>Insert date using codeIgniter</h3>
-
+				<form method="post" action="<?php echo base_url()?>main/form_validation">
+					<div class="form-group">
+						<label>Enter First Name</label>
+						<input type="text" name="first_name" class="form-control">
+						<span class="text-danger"><?php echo form_error('first_name');?></span>
+					</div>
+					<div class="form-group">
+						<label>Enter Last Name</label>
+						<input type="text" name="last_name" class="form-control">
+						<span class="text-danger"><?php echo form_error('last_name');?></span>
+					</div>
+				</form>
 			</div>
 	</body>
 </html>
